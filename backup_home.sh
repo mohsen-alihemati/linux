667777c665f4a1d2390831cf945eb4a75a3c8e5d
Mohsen Alihemati <Mohsen_alihemati@gmail.com>
2024-10-28 23:00:16 +0330

    insert scripts

---
+++ backup_home.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+BACKUP_DIR="/home/mohsen/script/backup"
+
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+
+PATH_USER="/home/$USER"
+
+
+tar -czf "$BACKUP_DIR/${USER}_home_backup_$DATE.tar.gz" "$PATH_USER"
